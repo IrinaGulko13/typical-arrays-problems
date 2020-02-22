95f371a5a4d8867bfc38a114abe02d8cd33711bc
Iryna Hulko <irinagulko13@gmail.com>
2020-02-22 17:32:00 +0300

    feat:typical-arrays-problem solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0)  {
+      return 0;
+  } else {
+      let min = array[0]
+      for (element of array) {
+          if (element < min) {
+              min = element;     
+          }  
+       }
+       return min; 
+  } 
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length === 0)  {
+        return 0;
+    } else {
+        let max = array[0]
+        for (element of array) {
+            if (element > max) {
+                max = element;     
+            }  
+         }
+        return max; 
+    } 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length === 0)  {
+        return 0;
+    } else {
+        let average = 0;
+        let sum = 0;
+        for (element of array) {
+            sum += element;
+            average = sum/array.length;
+        }
+        return average;
+    }
 }
